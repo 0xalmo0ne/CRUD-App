e04c92471b6e5f2fbfad587a77763a974d9c9936
0xalmo0ne <MEDO200t@gmail.com>
2025-11-07 21:55:43 +0200

    update responsive layout and center cards

--- JS/Main.js
+++ JS/Main.js
@@ -15,6 +15,7 @@ function addProduct() {
   }
   productContainear.push(proudact);
   clearForme()
+  displayProduct()
     console.log(productContainear);
       Swal.fire({
   title: "تم اضافة منتج بنجاح",
@@ -30,4 +31,21 @@ function clearForme() {
   category.value = null
   discrption.value = null
   image.value = null
+}
+
+function displayProduct() {
+
+  var cartoona = "";
+  for (let i = 0; i < productContainear.length; i++) {
+    cartoona+=`  <div class="col-md-2 ">
+  <div class="mx-auto proudact mt-5 border border-2 rounded-3 shadow-lg py-2 px-1">
+<img src="./Image/zoro-roronoa-one-piece-5k-io.jpg" class="w-100 rounded-3" alt="">
+<h5><span>Name :</span> Iphone 13 pro max</h5>
+<h5><span>Price :</span>33000</h5>
+<h5><span>Category :</span> Phone</h5>
+<h5><span>Discrption :</span> very good</h5>
+  </div>
+  </div>`
+  }
+  document.getElementById("rowData").innerHTML=cartoona
 }

454bbc3eaac0a4a02be3ac8bb1fd384a3e44f3cc
0xalmo0ne <MEDO200t@gmail.com>
2025-11-08 22:07:01 +0200

    some edite

--- JS/Main.js
+++ JS/Main.js
@@ -5,6 +5,14 @@ var discrption= document.getElementById("ProudactDiscrption");
 var image = document.getElementById("ProudactImage");
 
 var productContainear = [];
+
+if (localStorage.getItem("products") === null) {
+  productContainear = null;
+} else {
+  productContainear = JSON.parse(localStorage.getItem("products"));
+         displayProduct()
+}
+
 function addProduct() {
   var proudact = {
     code: nameInput.value,
@@ -16,6 +24,7 @@ function addProduct() {
   productContainear.push(proudact);
   clearForme()
   displayProduct()
+  localStorage.setItem(`products`, JSON.stringify(productContainear));
     console.log(productContainear);
       Swal.fire({
   title: "تم اضافة منتج بنجاح",
@@ -34,16 +43,15 @@ function clearForme() {
 }
 
 function displayProduct() {
-
   var cartoona = "";
   for (let i = 0; i < productContainear.length; i++) {
-    cartoona+=`  <div class="col-md-2 ">
+    cartoona+=`  <div class="col-md-2 col-sm-8 col-lg-3 mb-3">
   <div class="mx-auto proudact mt-5 border border-2 rounded-3 shadow-lg py-2 px-1">
 <img src="./Image/zoro-roronoa-one-piece-5k-io.jpg" class="w-100 rounded-3" alt="">
-<h5><span>Name :</span> Iphone 13 pro max</h5>
-<h5><span>Price :</span>33000</h5>
-<h5><span>Category :</span> Phone</h5>
-<h5><span>Discrption :</span> very good</h5>
+<h5><span class="fw-bold">Name :</span>${productContainear[i].code}</h5>
+<h5><span class="fw-bold">Price :</span>${productContainear[i].price}</h5>
+<p><span class="fs-5 fw-bold">Category :</span>${productContainear[i].category} </p>
+<h5><span class="fw-bold">Discrption :</span>${productContainear[i].discrption}</h5>
   </div>
   </div>`
   }
